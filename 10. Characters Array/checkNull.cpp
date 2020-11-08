666174d04c5298ea2fe928944bd7bae91c6d57ad
Madhav Anand <madhavanand402@gmail.com>
2020-11-08 22:17:25 +0530

    Update 1

---
+++ 10. Characters Array/checkNull.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //Declaring and Initializing Character Array
+    char fruitName[20]="Mango";
+
+    //Loop to traverse all the character till null
+    for (int i = 0; fruitName[i] != '\0'; i++)
+        cout<<fruitName[i];
+
+    return 0;
+    
+}
---
+++ 10. Characters Array/inputOutput.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //Declaring a character array
+    char name[20];
+
+    //Taking Input from User
+    cin>>name;
+
+    //Printing Input as it is
+    cout<<name<<endl;
+
+    //Printing First Character of the Input
+    cout<<name[0]<<endl;
+
+    return 0;
+}
