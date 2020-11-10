b97f1762f1584c2921b9ae9de72fad18916734f8
Madhav Anand <madhavanand402@gmail.com>
2020-11-10 07:35:58 +0530

    Get Started

---
+++ 11. Pointers/getStarted.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //Declare and Initialize variable and pointer
+    int a = 10;
+    int *pointer;
+    pointer = &a;
+
+    cout<<&a<<endl; // Prints the address of variable a
+    cout<<pointer<<endl; // Prints the address of variable a stored in pointer
+    cout<<*pointer<<endl; // Prints the value of a via pointer
+
+    return 0;
+}
