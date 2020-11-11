7b68273ad034d9256add1ab2023c0a498bfd0422
Madhav Anand <madhavanand402@gmail.com>
2020-11-11 08:45:10 +0530

    Stacks Added

---
+++ 12. Stacks Vs Heaps/stacks.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int a, int b){
+    //Two local variables of add function - a, b.
+    return a + b;
+}
+
+int main(){
+
+    //Three Local variables of main function - a, b and sum.
+    int a=2; 
+    int b=2;
+    int sum = add(a,b);
+    cout<<sum<<endl;
+    return 0;
+}
