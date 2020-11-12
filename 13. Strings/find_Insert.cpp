069670f2252263e853480b93ee7b171133959a21
Madhav Anand <madhavanand402@gmail.com>
2020-11-12 08:29:16 +0530

    Find Insert Added

---
+++ 13. Strings/find_Insert.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string str = "GitHub";
+    //Find
+    cout<<str.find("Hub")<<endl;
+
+    //Insert
+    cout<<str.insert(0,"Microsoft ")<<endl;
+    
+    return 0;
+}
