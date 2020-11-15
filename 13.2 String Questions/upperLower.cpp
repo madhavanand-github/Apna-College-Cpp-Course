a1c571af86c33cf499050fc27238ef9a9e8b88e8
Madhav Anand <madhavanand402@gmail.com>
2020-11-15 08:30:18 +0530

    Letter Cases Added

--- 13.2 String Questions/upperLower.cpp
+++ 13.2 String Questions/upperLower.cpp
@@ -3,12 +3,31 @@ using namespace std;
 
 int main(){
 
-    //Assumption #1 all lowerCase
-    string str1 = "madhav";
-    for (int i = 0; i < str1.length(); i++)
-    {
-        /* code */
-    }
     
+    string str = "GitHub";
+
+    //To Uppercase
+    for (int i = 0; i < str.length(); i++)
+        if(str[i]>='a'&& str[i]<='z')
+            str[i]-=32;
+    cout<<str<<endl;
+
+    //To Lowercase
+    for (int i = 0; i < str.length(); i++)
+        if(str[i]>='A'&& str[i]<='Z')
+            str[i]+=32;
+    cout<<str<<endl;
+    
+    str="GitHub";
+    //Toggle Case
+    for (int i = 0; i < str.length(); i++)
+        if(str[i]>='a'&& str[i]<='z')
+            str[i]-=32;
+        else if(str[i]>='A'&& str[i]<='Z')
+            str[i]+=32;
+
+    cout<<str<<endl;
+    
+
     return 0;
 }
