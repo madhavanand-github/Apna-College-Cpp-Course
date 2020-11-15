405ff7eae16fa7f14796ed4357ef393f48be2ac2
Madhav Anand <madhavanand402@gmail.com>
2020-11-15 08:51:54 +0530

    Completed

--- 13.2 String Questions/upperLower.cpp
+++ 13.2 String Questions/upperLower.cpp
@@ -25,9 +25,14 @@ int main(){
             str[i]-=32;
         else if(str[i]>='A'&& str[i]<='Z')
             str[i]+=32;
-
     cout<<str<<endl;
     
+    // With Inbuilt Function
+    string str1 = "Microsoft";
+    transform(str1.begin(), str1.end(), str1.begin(), ::toupper);
+    cout<<str1<<endl;
+    transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
+    cout<<str1<<endl;
 
     return 0;
 }
