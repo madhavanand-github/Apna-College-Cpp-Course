87ce5797923747e444ed14d46be9b56276c7a051
Madhav Anand <madhavanand402@gmail.com>
2020-11-16 08:09:16 +0530

    Doubt Added

---
+++ 13.1 String Challenges/doubt.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string s;
+    string s1;
+
+    // Input - Microsoft GitHub{/enter}
+    cin>>s;
+    cin.ignore();
+
+    cin>>s1;
+    cin.ignore();
+
+    cout<<s;
+    cout<<s1;
+
+    /* 
+    1. Why cin.ignore is not working in this case ?
+    Output - Microsoft itHub
+    */
+
+    return 0;
+}
