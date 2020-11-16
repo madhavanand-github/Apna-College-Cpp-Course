1cf96caa04520722a87107a8d196e7665bcdaca1
Madhav Anand <madhavanand402@gmail.com>
2020-11-16 10:06:29 +0530

    Set Bit Updated

--- 14 Bit Manipulation/setBit.cpp
+++ 14 Bit Manipulation/setBit.cpp
@@ -1,13 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int setBit(int n, int pos){
+int setBit_1(int n, int pos){
     return ( n | (1<<pos));
+}
+
+int setBit_0(int n, int pos){
+    return ( n & ~(1<<pos));
 }
 
 int main(){
 
-    cout<<setBit(5,1);
+    cout<<setBit_1(5,1);
+    cout<<setBit_0(5,0);
 
     return 0;
 }
